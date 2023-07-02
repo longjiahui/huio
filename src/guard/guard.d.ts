a08d40ee1e01037084358b81b59fe902213f9d35
anfo <longjiahui@hotmail.com>
2023-07-02 23:08:04 +0800

    chore: delete useless files

--- src/guard/guard.d.ts
+++
@@ -1,7 +0,0 @@
-import { Socket } from 'socket.io'
-import { ExtendedError } from 'socket.io/dist/namespace'
-
-export type Middleware = (
-    socket: Socket,
-    next: (err?: ExtendedError) => void,
-) => void
